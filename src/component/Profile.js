24b65a3019346ec31bf20247c590204d8a2e9d24
DonHorse <hermel.flo@gmail.com>
2022-04-18 22:37:52 +0200

    update

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -5,6 +5,7 @@
 import '../style/App.css'
 import {useEffect, useState} from "react";
 import Axios from "axios";
+import React from "react";
 
 
 // fonction du pied de page
@@ -18,10 +19,10 @@ function Profile() {
     const [height, setHeight] = useState(0);
     const [contraindication, setContraindication] = useState('');
     const [note, setNote] = useState('');
-    const [trNumber, setTrNumber] = useState(0);
+    //const [trNumber, setTrNumber] = useState(0);
     const [img, setImg] = useState('');
-    const [lastUpdate, setLastUpdate] = useState('');
-    const [PlaceholderInfo, setPlaceholderInfo ] = useState('');
+    //const [lastUpdate, setLastUpdate] = useState('');
+    const [PlaceholderInfo, setPlaceholderInfo ] = useState([]);
 
     Axios.defaults.withCredentials = true;
 
@@ -35,127 +36,107 @@ function Profile() {
                 window.location.href = "/Login";
             }
         });
-    });
+    }, [userId]);
     // requête à l'API POST (GET des messages en placeholder et UPDATE une fois validé )
 
     useEffect(() => {
-        Axios.get("http://localhost:3001/DIPSS/profile/:").then((response) => {
+        Axios.get("http://localhost:3001/DIPSS/profile",{
+            userId : userId
+        }).then((response) => {
             setPlaceholderInfo(response.data);
             console.log(response.data);
-        });
-    });
+        })
+    },[userId]);
 
-    // const addAdminModifTitle = () => {
-    //     Axios.put('http://localhost:3001/api/homeModif/title', {
-    //         bodyTitle : bodyTitle,
-    //     }).then(() => {
-    //         alert("modifications enregistrées !");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
-    //
-    // const addAdminModifBody1 = () => {
-    //     Axios.put('http://localhost:3001/api/homeModif/body1', {
-    //         body1 : body1
-    //     }).then(() => {
-    //         alert("modifications enregistrées !");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
-    //
-    // const addAdminModifFooter1 = () => {
-    //     Axios.put('http://localhost:3001/api/homeModif/footer1', {
-    //         footer1 : footer1
-    //     }).then(() => {
-    //         alert("modifications enregistrées !");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
-    //
-    // const addAdminModifFooter2 = () => {
-    //     Axios.put('http://localhost:3001/api/homeModif/footer2', {
-    //         footer2 : footer2
-    //     }).then(() => {
-    //         alert("modifications enregistrées !");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
-    //
-    // const addAdminModifFooter3 = () => {
-    //     Axios.put('http://localhost:3001/api/homeModif/footer3', {
-    //         footer3 : footer3
-    //     }).then(() => {
-    //         alert("modifications enregistrées !");
-    //     }).catch((error) => {
-    //         console.log(error);
-    //     });
-    // };
+    const updateProfile = () => {
+        Axios.put('http://localhost:3001/DIPSS/profile/update', {
+            userId : userId,
+            gender : gender,
+            birthday : birthday,
+            weight : weight,
+            height : height,
+            contraindication : contraindication,
+            note : note,
+            img : img
+        }).then(() => {
+            alert("modifications enregistrées !");
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
 
 
     // Affichage du formulaire
-    // return(
-    //     <div className="admin-form">
-    //         <form>
-    //             <label htmlFor="title">Titre de page : </label>
-    //             <input
-    //                 type="text"
-    //                 name="bodyTitle"
-    //                 id="bodyTitle"
-    //                 placeholder= {PlaceholderInfo.map((val) => { return (val.bodyTitle)})}
-    //                 onChange={(e) => {
-    //                     setBodyTitle(e.target.value);
-    //                 }}/>
-    //             <button type="submit" onClick={addAdminModifTitle}>Valider les modifications</button>
-    //
-    //             <label htmlFor="text">Message d'accueil : </label>
-    //             <textarea
-    //                 name="body1"
-    //                 id="body1"
-    //                 placeholder= {PlaceholderInfo.map((val) => { return (val.body1)})}
-    //                 onChange={(e) => {
-    //                     setBody1(e.target.value);
-    //                 }}
-    //             />
-    //             <button type="submit" onClick={addAdminModifBody1}>Valider les modifications</button>
-    //
-    //             <label htmlFor="text">Pied de page gauche : </label>
-    //             <textarea
-    //                 name="footer1"
-    //                 id="footer1"
-    //                 placeholder= {PlaceholderInfo.map((val) => { return (val.footer1)})}
-    //                 onChange={(e) => {
-    //                     setFooter1(e.target.value);
-    //                 }}/>
-    //             <button type="submit" onClick={addAdminModifFooter1}>Valider les modifications</button>
-    //
-    //             <label htmlFor="text">Pied de page centre : </label>
-    //             <textarea
-    //                 name="footer2"
-    //                 id="footer2"
-    //                 placeholder={PlaceholderInfo.map((val) => { return (val.footer2)})}
-    //                 onChange={(e) => {
-    //                     setFooter2(e.target.value);
-    //                 }}/>
-    //             <button type="submit" onClick={addAdminModifFooter2}>Valider les modifications</button>
-    //
-    //             <label htmlFor="text">Pied de page droite : </label>
-    //             <textarea
-    //                 name="footer3"
-    //                 id="footer3"
-    //                 placeholder= {PlaceholderInfo.map((val) => { return (val.footer3)})}
-    //                 onChange={(e) => {
-    //                     setFooter3(e.target.value);
-    //                 }}/>
-    //             <button type="submit" onClick={addAdminModifFooter3}>Valider les modifications</button>
-    //
-    //             <input type="reset"/>
-    //         </form>
-    //     </div>
-    // )
+    return(
+        <div className="admin-form">
+            <form>
+                <label htmlFor="title">Titre de page : </label>
+
+                <label>Genre :</label>
+                <select
+                    placeholder= {PlaceholderInfo.map((val) => { return (val.gender)})}
+                    onChange={(e) => {
+                    setGender(e.target.value); }}>
+                    <option value='M'>Homme</option>
+                    <option value='F'>Femme</option>
+                </select>
+
+                <input
+                    type="date"
+                    name="birthday"
+                    id="birthday"
+                    placeholder= {PlaceholderInfo.map((val) => { return (val.birthday)})}
+                    onChange={(e) => {
+                        setBirthday(e.target.value);
+                    }}/>
+                <input
+                    type="number"
+                    name="weight"
+                    id="weight"
+                    placeholder= {PlaceholderInfo.map((val) => { return (val.weight)})}
+                    onChange={(e) => {
+                        setWeight(e.target.value);
+                    }}/>
+                <input
+                    type="number"
+                    name="height"
+                    id="height"
+                    placeholder= {PlaceholderInfo.map((val) => { return (val.height)})}
+                    onChange={(e) => {
+                        setHeight(e.target.value);
+                    }}/>
+                <label htmlFor="text">Contre-indication(s) : </label>
+                <textarea
+                    name="contraindication"
+                    id="contraindication"
+                    placeholder={PlaceholderInfo.map((val) => { return (val.contraindication)})}
+                    onChange={(e) => {
+                        setContraindication(e.target.value);
+                    }}/>
+                <label htmlFor="text">Notes divers: </label>
+                <textarea
+                    name="note"
+                    id="note"
+                    placeholder={PlaceholderInfo.map((val) => { return (val.note)})}
+                    onChange={(e) => {
+                        setNote(e.target.value);
+                    }}/>
+                <input
+                    type="text"
+                    name="image"
+                    id="image"
+                    placeholder= {PlaceholderInfo.map((val) => { return (val.img)})}
+                    onChange={(e) => {
+                        setImg(e.target.value);
+                    }}/>
+
+                <button type="submit" onClick={updateProfile}>Valider les modifications</button>
+
+                <input type="reset"/>
+            </form>
+        </div>
+    )
 }
 
 export default Profile;
